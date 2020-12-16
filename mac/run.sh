4376c1236a92c115c2452ad379a1c3ab87be8ed3
Disi A <adis@live.cn>
2020-12-16 16:05:33 +0800

    Go static.

--- mac/run.sh
+++ mac/run.sh
@@ -1,5 +1,6 @@
-rm *.a
-g++ -std=c++11 -I ../include -L ../mnn-mac -lMNN -o main.a main.cpp -rpath @executable_path #-lMNN_Express
+rm main.a
+g++ -Bstatic -std=c++11 -I ../include -L ../mnn-mac/static -lMNN -o main.a main.cpp #-rpath @executable_path #-lMNN_Express
+#g++ -std=c++11 -I ../include -L ../mnn-mac -lMNN -o main.a main.cpp -rpath @executable_path #-lMNN_Express
 
 # install_name_tool -change @rpath/libMNN_Express.dylib @executable_path/`basename libMNN_Express.dylib` ./main.a
 #./main.a ../model/model-mobilenet_v1_075.mnn
